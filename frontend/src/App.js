1fd677ce6eb0e09487f8d197b7195ded4efbd05f
Tony Ma <officialtonyma@gmail.com>
2022-01-27 23:29:33 -0500

    login and signup pages link to eachother!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { PublicProfilePage } from "./pages/PublicProfilePage";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { SignupPage } from "./pages/SignupPage";
+import { LoginPage } from "./pages/LoginPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/signup" element={<SignupPage />}></Route>
+        <Route exact path="/login" element={<LoginPage />}></Route>
         <Route exact path="/edit" element={<PrivateEditingPage />}></Route>
       </Routes>
     </Router>
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const SignupPage = () => {
+export const LoginPage = () => {
   return (
     <>
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -12,7 +12,7 @@ export const SignupPage = () => {
               alt="Workflow"
             />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Sign Up for an account
+              Login to your account
             </h2>
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST">
@@ -48,20 +48,29 @@ export const SignupPage = () => {
               </div>
             </div>
 
+            <div className="flex items-center justify-end text-sm">
+              <a
+                href="#"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Forgot your password?
+              </a>
+            </div>
+
             <div>
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                Create account
+                Login
               </button>
             </div>
             <div className="flex items-center justify-center text-sm">
               <a
-                href="/login"
+                href="/signup"
                 className="flex items-center font-medium text-indigo-600 hover:text-indigo-500"
               >
-                Already have an acccount? Login
+                No account yet? Sign Up
               </a>
             </div>
           </form>
