4bf58fa96192ed798cdc2be2b89cd9611d83e5fa
Tony Ma <officialtonyma@gmail.com>
2022-01-27 23:18:41 -0500

    finally signup page template renders correctly lol

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,13 @@ import { PrivateEditingPage } from "./pages/PrivateEditingPage";
 import { PublicProfilePage } from "./pages/PublicProfilePage";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SignupPage } from "./pages/SignupPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/signup">
-          <About />
-        </Route>
-        <Route path="/login">
-          <Home />
-        </Route> */}
+        <Route exact path="/signup" element={<SignupPage />}></Route>
         <Route exact path="/edit" element={<PrivateEditingPage />}></Route>
       </Routes>
     </Router>
