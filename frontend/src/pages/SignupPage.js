ca05d57436df832eafbc44bc6342ea20883245ba
Tony Ma <officialtonyma@gmail.com>
2022-01-28 22:37:15 -0500

    slightly better error msg formatting

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -37,7 +37,7 @@ export const SignupPage = () => {
               Sign Up for an account
             </h2>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <form className="mt-8" action="#" method="POST">
             <div className="rounded-md relative shadow-sm">
               <input
                 id="username"
@@ -46,20 +46,26 @@ export const SignupPage = () => {
                   usernameIsValid(event.target.value);
                 }}
                 className="pl-32 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                style={{ paddingLeft: "8.3rem" }}
               />
               <div className="absolute items-center left-3 top-0 bottom-0 z-50 flex">
                 <LinkIcon className="h-6 w-6 text-indigo-600" />
-                <p className="pl-1 tracking-wider text-sm font-bold text-gray-700">
+                <p className="pl-1 tracking-wide font-bold text-gray-700">
                   1feed.com/
                 </p>
               </div>
             </div>
 
             {usernameErrorMsg !== "valid" && (
-              <p className="text-sm text-red-500">{usernameErrorMsg}</p>
+              <p
+                className="text-red-500 ml-2 mb-5"
+                style={{ fontSize: "13px" }}
+              >
+                {usernameErrorMsg}
+              </p>
             )}
 
-            <div className="rounded-md shadow-sm -space-y-px">
+            <div className="rounded-md shadow-sm -space-y-px mb-6">
               <div>
                 <label htmlFor="email-address" className="sr-only">
                   Email address
@@ -90,7 +96,7 @@ export const SignupPage = () => {
               </div>
             </div>
 
-            <div>
+            <div className="mb-6">
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
