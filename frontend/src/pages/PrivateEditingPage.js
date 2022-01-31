0a5da3bc3f0c00d6a06628da7f9643910fc55a74
Tony Ma <officialtonyma@gmail.com>
2022-01-30 23:39:10 -0500

    dynamic URLs seem to work!

--- frontend/src/pages/PrivateEditingPage.js
+++ frontend/src/pages/PrivateEditingPage.js
@@ -7,6 +7,7 @@ import Modal from "react-modal";
 import { linkYouTube } from "../services/linkPlatforms";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Link } from "react-router-dom";
 
 export const PrivateEditingPage = () => {
   const allSocialMedias = [
@@ -33,13 +34,13 @@ export const PrivateEditingPage = () => {
   const returnCorrectIcon = (objName) => {
     switch (objName) {
       case "YouTube":
-        return <BsYoutube size={30} class="absolute left-3" />;
+        return <BsYoutube size={30} className="absolute left-3" />;
       case "Facebook":
-        return <BsFacebook size={26} class="absolute left-3" />;
+        return <BsFacebook size={26} className="absolute left-3" />;
       case "Instagram":
-        return <BsInstagram size={26} class="absolute left-3" />;
+        return <BsInstagram size={26} className="absolute left-3" />;
       case "Twitter":
-        return <BsTwitter size={26} class="absolute left-3" />;
+        return <BsTwitter size={26} className="absolute left-3" />;
       default:
         break;
     }
@@ -59,23 +60,25 @@ export const PrivateEditingPage = () => {
   const [bioModalVisible, setBioModalVisible] = useState(false);
   const [ytModalVisible, setYTModalVisible] = useState(false);
 
+  const fakeUsername = "demar";
+
   return (
-    <div class="flex flex-col justify-center items-center ">
-      <h1 class="mt-5 text-3xl font-bold">Private Editing Page</h1>
-      <img class="rounded-full w-24 h-24 mt-8" src={avi} alt="pfp" />
-      <h3 class="mt-1 font-semibold text-lg">@DeMar_Derozan</h3>
+    <div className="flex flex-col justify-center items-center ">
+      <h1 className="mt-5 text-3xl font-bold">Private Editing Page</h1>
+      <img className="rounded-full w-24 h-24 mt-8" src={avi} alt="pfp" />
+      <h3 className="mt-1 font-semibold text-lg">@DeMar_Derozan</h3>
       <button
         onClick={() => setBioModalVisible(true)}
         onMouseOver={() => setIsHoveringBio(true)}
         onMouseLeave={() => setIsHoveringBio(false)}
-        class="mt-2 text-sm w-96 text-center p-1 hover:bg-gray-100 hover:rounded-xl relative"
+        className="mt-2 text-sm w-96 text-center p-1 hover:bg-gray-100 hover:rounded-xl relative"
         style={{ wordBreak: "break-word" }}
       >
         {bio}
         {isHoveringBio && (
           <AiFillEdit
             size={20}
-            class="absolute bottom-1 right-1 text-gray-800"
+            className="absolute bottom-1 right-1 text-gray-800"
           />
         )}
       </button>
@@ -84,16 +87,16 @@ export const PrivateEditingPage = () => {
         bioModalVisible={bioModalVisible}
         setBioModalVisible={setBioModalVisible}
       />
-      <button class="mt-2 text-blue-500 text-xs flex flex-row items-center">
-        view your public profile
+      <button className="mt-2 text-blue-500 text-xs flex flex-row items-center">
+        <Link to={`/${fakeUsername}`}>view your public profile</Link>
         <IoIosShareAlt size={20} class="pb-0.5 ml-1" />
       </button>
-      <div class="mt-14 flex flex-col">
+      <div className="mt-14 flex flex-col">
         {allSocialMedias.map((obj) => (
           <button
             disabled={isLinkedMap[obj.name]}
             onClick={() => openCorrectModal(obj.name)}
-            class={
+            className={
               isLinkedMap[obj.name]
                 ? styles.disabledButton
                 : styles.enabledButton
@@ -123,13 +126,13 @@ const EditBioModal = ({ ...props }) => {
       style={styles.modal}
       ariaHideApp={false}
     >
-      <div class="flex items-center flex-col">
-        <label class="mt-8 text-gray-800 font-semibold text-xl">
+      <div className="flex items-center flex-col">
+        <label className="mt-8 text-gray-800 font-semibold text-xl">
           New bio:
           <textarea
             type="text"
             maxLength="140"
-            class="border-2 text-sm w-96 h-40 flex rounded-xl p-2"
+            className="border-2 text-sm w-96 h-40 flex rounded-xl p-2"
             placeholder="Limit 140 characters."
             onChange={(event) => setNewBioText(event.target.value)}
           />
@@ -137,7 +140,7 @@ const EditBioModal = ({ ...props }) => {
         <div>
           <button
             onClick={() => props.setBioModalVisible(false)}
-            class="rounded-full bg-gray-300 m-2 py-2 px-4"
+            className="rounded-full bg-gray-300 m-2 py-2 px-4"
           >
             Cancel
           </button>
@@ -146,7 +149,7 @@ const EditBioModal = ({ ...props }) => {
               props.setBio(newBioText);
               props.setBioModalVisible(false);
             }}
-            class="rounded-full bg-gray-300 m-2 py-2 px-4 bg-blue-600 text-white"
+            className="rounded-full bg-gray-300 m-2 py-2 px-4 bg-blue-600 text-white"
           >
             Save
           </button>
@@ -164,16 +167,16 @@ const LinkYouTubeModal = ({ ...props }) => {
       style={styles.modal}
       ariaHideApp={false}
     >
-      <div class="flex items-center flex-col">
-        <label class="mt-8 text-gray-800 font-semibold text-xl">
+      <div className="flex items-center flex-col">
+        <label className="mt-8 text-gray-800 font-semibold text-xl">
           Enter your YouTube channel name: <br />
-          <div class="flex flex-row items-center">
-            <span class="text-sm font-normal mr-1">
+          <div className="flex flex-row items-center">
+            <span className="text-sm font-normal mr-1">
               https://www.youtube.com/channel/
             </span>
             <input
               type="text"
-              class="border-2 text-sm w-96 h-10 flex rounded-xl p-2"
+              className="border-2 text-sm w-96 h-10 flex rounded-xl p-2"
               placeholder="UC-lHJZR3Gqxm24_Vd_AJ5Yw (must start with UC)"
               onChange={(event) => setChannelId(event.target.value)}
             />
@@ -182,7 +185,7 @@ const LinkYouTubeModal = ({ ...props }) => {
         <div>
           <button
             onClick={() => props.setYTModalVisible(false)}
-            class="rounded-full bg-gray-300 m-2 py-2 px-4"
+            className="rounded-full bg-gray-300 m-2 py-2 px-4"
           >
             Cancel
           </button>
@@ -203,7 +206,7 @@ const LinkYouTubeModal = ({ ...props }) => {
                 toast("Invalid channel ID");
               }
             }}
-            class="rounded-full bg-gray-300 m-2 py-2 px-4 bg-blue-600 text-white"
+            className="rounded-full bg-gray-300 m-2 py-2 px-4 bg-blue-600 text-white"
           >
             Link
           </button>
