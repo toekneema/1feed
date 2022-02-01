4fd5261d03481ac65eb5941818803b40bbff20c5
Tony Ma <officialtonyma@gmail.com>
2022-01-31 22:20:02 -0500

    fixed case insensitive @username on public page, now it has case sensitive formatting lets go!

--- frontend/src/pages/PublicProfilePage.js
+++ frontend/src/pages/PublicProfilePage.js
@@ -3,23 +3,12 @@ import avi from "../assets/images/twitter_egg.png";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
 
-const usernameSet = new Set([
-  "josh",
-  "adam",
-  "tony",
-  "armaan",
-  "demar",
-  "steph",
-]);
-
 export const PublicProfilePage = () => {
   const { username } = useParams();
   const username_lowercase = username.toLowerCase();
   const { data, loading } = useFetch(
     `http://localhost:1337/api/users?filters[username_lowercase][$eq]=${username_lowercase}`
   );
-
-  console.log("what is data obj when username doesnt exist", data);
 
   if (data === null || data.length === 0) {
     return (
@@ -28,6 +17,7 @@ export const PublicProfilePage = () => {
       </div>
     );
   }
+  const username_case_sensitive = data[0].username;
 
   return (
     <>
@@ -37,7 +27,9 @@ export const PublicProfilePage = () => {
         <div className="flex flex-col justify-center items-center ">
           <h1 className="mt-5 text-3xl font-bold">Public Profile Page</h1>
           <img className="rounded-full w-24 h-24 mt-8" src={avi} alt="pfp" />
-          <h3 className="mt-1 font-semibold text-lg">@{username}</h3>
+          <h3 className="mt-1 font-semibold text-lg">
+            @{username_case_sensitive}
+          </h3>
           <div
             className="mt-2 text-sm w-96 text-center p-1"
             style={{ wordBreak: "break-word" }}
