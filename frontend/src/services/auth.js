8d19467665103d4af5b7ac4bdcca625cd22d8d5b
Tony Ma <officialtonyma@gmail.com>
2022-02-03 11:51:49 -0500

    fix some signup auth syntax

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -4,23 +4,56 @@ export const register = async (username, email, password) => {
   let error = null;
   let hasError = false;
   try {
-    const res = await fetch("http://localhost:1337/api/auth/local/register", {
-      username: username,
-      email: email,
-      password: password,
-    });
-    const json = await res.json();
-    global.userObj = json.data;
+    const userInfo = await (
+      await fetch("http://localhost:1337/api/auth/local/register", {
+        method: "POST",
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+    ).json();
+    global.userObj = userInfo;
     console.log("Signup success!");
-    console.log("User profile", json.data.user);
   } catch (e) {
-    console.log("Signup Error:", e.response);
+    console.log("Signup Error:", e);
     error = `Signup Error. ${e}.`;
     hasError = true;
   }
-  console.log(global.userObj, "what is globalUserObj");
   return [error, hasError];
 };
+
+export const login = async (email, password) => {
+  let error = null;
+  let hasError = false;
+  try {
+    const userInfo = await (
+      await fetch("http://localhost:1337/api/auth/local", {
+        method: "POST",
+        body: JSON.stringify({
+          identifier: email,
+          password: password,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+    ).json();
+    global.userObj = userInfo;
+    console.log("Login success!");
+  } catch (e) {
+    console.log("Login Error:", e);
+    error = `Login Error. ${e}.`;
+    hasError = true;
+  }
+  return [error, hasError];
+};
+
+// example of .then.then, does the same thing, just doesn't need to use async/await keywords
 
 // export const login = async (email, password) => {
 //   let error = null;
@@ -48,29 +81,3 @@ export const register = async (username, email, password) => {
 //       return [error, hasError];
 //     });
 // };
-
-export const login = async (email, password) => {
-  let error = null;
-  let hasError = false;
-  try {
-    const userInfo = await (
-      await fetch("http://localhost:1337/api/auth/local", {
-        method: "POST",
-        body: JSON.stringify({
-          identifier: email,
-          password: password,
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      })
-    ).json();
-    global.userObj = userInfo;
-    console.log("Login success!");
-  } catch (e) {
-    console.log("Login Error:", e);
-    error = `Login Error. ${e}.`;
-    hasError = true;
-  }
-  return [error, hasError];
-};
