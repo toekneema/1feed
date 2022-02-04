27871027e3b9945392dbe87361e3a13ed2a576ec
Tony Ma <officialtonyma@gmail.com>
2022-02-04 11:13:04 -0500

    conditionally sends user to /edit or /login based on whether they are logged in or not

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,32 @@
 import { PrivateEditingPage } from "./pages/PrivateEditingPage";
 import { PublicProfilePage } from "./pages/PublicProfilePage";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { SignupPage } from "./pages/SignupPage";
 import { LoginPage } from "./pages/LoginPage";
 
 function App() {
+  const isLoggedIn = localStorage.getItem("jwt") ? true : false;
+
   return (
     <Router>
       <Routes>
-        <Route exact path="/signup" element={<SignupPage />}></Route>
-        <Route exact path="/login" element={<LoginPage />}></Route>
-        <Route exact path="/edit" element={<PrivateEditingPage />}></Route>
-        <Route exact path="/:username" element={<PublicProfilePage />}></Route>
+        <Route
+          exact
+          path="/"
+          element={
+            isLoggedIn ? <Navigate to="/edit" /> : <Navigate to="/login" />
+          }
+        ></Route>
+        <Route exact path="/signup" element={<SignupPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/edit" element={<PrivateEditingPage />} />
+        <Route exact path="/:username" element={<PublicProfilePage />} />
       </Routes>
     </Router>
   );
