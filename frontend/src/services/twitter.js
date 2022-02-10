3df8e5d3df1e81165b621d5cafbfcc4e408ff7e7
Tony Ma <officialtonyma@gmail.com>
2022-02-10 00:31:08 -0500

    create new dir for the misc-backend REST endpoints

--- frontend/src/services/twitter.js
+++ frontend/src/services/twitter.js
@@ -2,6 +2,7 @@ export const getTwitter = async (twitterUsername) => {
   const theme = "light";
   let [data, hasError] = [null, false];
   const requestURL = `https://publish.twitter.com/oembed?url=https://twitter.com/${twitterUsername}`;
+  // const requestURL = `http://localhost:1337/api/twitter/${twitterUsername}`
   try {
     data = await (
       await fetch(requestURL, {
