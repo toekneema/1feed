85b7ff5c714717e2b5b18a73e56312d9ab53118b
Tony Ma <officialtonyma@gmail.com>
2022-02-10 23:41:37 -0500

    heroku first commit

--- frontend/src/services/twitter.js
+++ frontend/src/services/twitter.js
@@ -1,15 +1,10 @@
 export const getTwitter = async (twitterUsername) => {
   const theme = "light";
   let [data, hasError] = [null, false];
-  const requestURL = `https://publish.twitter.com/oembed?url=https://twitter.com/${twitterUsername}`;
-  // const requestURL = `http://localhost:1337/api/twitter/${twitterUsername}`
+  const requestURL = `http://localhost:8080/twitter/${twitterUsername}`;
   try {
-    data = await (
-      await fetch(requestURL, {
-        method: "GET",
-        mode: "cors",
-      })
-    ).json();
+    data = await (await fetch(requestURL)).json();
+    console.log("what is data lol ✅✅✅", data);
   } catch (e) {
     console.log("Error fetching Twitter data:", e);
     hasError = true;
