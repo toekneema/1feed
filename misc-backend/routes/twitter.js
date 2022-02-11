ca7f594242b8ca991dea0070ff113034b7c81045
Tony Ma <officialtonyma@gmail.com>
2022-02-10 23:40:22 -0500

    push

--- misc-backend/routes/twitter.js
+++ misc-backend/routes/twitter.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 
 // Get all tweets using passed in Twitter username
 router.get("/:id", (req, res) => {
-  res.send("Hello World");
+  let [data, hasError] = [null, false];
+  const requestURL = `https://publish.twitter.com/oembed?url=https://twitter.com/${req.params.id}`;
+  axios
+    .get(requestURL)
+    .then((response) => {
+      res.send(response.data.html);
+    })
+    .catch((e) => {
+      hasError = true;
+      res.send(`Error fetching Twitter data: ${e}`);
+    });
 });
 
 module.exports = router;
