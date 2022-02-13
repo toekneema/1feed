9ab9710146188f9fcb9ff11321c0990bfd14f9bf
Tony Ma <officialtonyma@gmail.com>
2022-02-13 01:15:22 -0500

    deletes the old account in real time on that modal! lets go!

--- frontend/src/components/LinkTwitterModal.js
+++ frontend/src/components/LinkTwitterModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { updateUser } from "../services/user";
 import Modal from "react-modal";
 import { styles } from "../styles";
@@ -7,6 +7,10 @@ import { getFormattedTwitterUsernameIfExists } from "../services/twitter";
 import { TrashIcon } from "@heroicons/react/solid";
 export const LinkTwitterModal = ({ ...props }) => {
   const [twitterUsername, setTwitterUsername] = useState("");
+  const [linksMapHook, setLinksMapHook] = useState(props.linksMap); // only necessary since i want to re-render based on this variable changing
+
+  useEffect(() => {}, [props.linksMap]);
+
   return (
     <Modal
       isOpen={props.twitterModalVisible}
@@ -17,10 +21,15 @@ export const LinkTwitterModal = ({ ...props }) => {
         <label className="mt-8 text-gray-800 font-semibold text-xl text-center">
           Enter your Twitter username:
         </label>
-        {props.linksMap.Twitter.length > 0 ? (
+        {linksMapHook.Twitter.length > 0 ? (
           <>
-            {props.linksMap.Twitter.map((username, idx) => (
-              <DisabledUsernameInput username={username} />
+            {linksMapHook.Twitter.map((username, idx) => (
+              <DisabledUsernameInput
+                key={idx}
+                username={username}
+                linksMapHook={linksMapHook}
+                setLinksMapHook={setLinksMapHook}
+              />
             ))}
             <UsernameInput setTwitterUsername={setTwitterUsername} />
           </>
@@ -46,22 +55,25 @@ export const LinkTwitterModal = ({ ...props }) => {
               if (hasError) {
                 toast("Username does not exist on Twitter.");
               } else if (
-                props.linksMap.Twitter.includes(formattedTwitterUsername)
+                linksMapHook.Twitter.includes(formattedTwitterUsername)
               ) {
                 toast("Cannot link the same username again.");
               } else {
                 const [userData, hasError] = await updateUser({
                   linksMap: {
-                    ...props.linksMap,
+                    ...linksMapHook,
                     Twitter: [
-                      ...props.linksMap.Twitter,
+                      ...linksMapHook.Twitter,
                       formattedTwitterUsername,
                     ],
                   },
                 });
                 props.setTwitterModalVisible(false);
-                props.setLinksMap(userData.linksMap);
-                toast("Successfully linked Twitter account!");
+                props.setLinksMap(userData.linksMap); // passes the data to parent (outside of modal)
+                setLinksMapHook(userData.linksMap); // used to update the current modal
+                toast(
+                  `Successfully linked Twitter account ${formattedTwitterUsername}!`
+                );
               }
             }}
           >
@@ -73,18 +85,36 @@ export const LinkTwitterModal = ({ ...props }) => {
   );
 };
 
-const DisabledUsernameInput = ({ username }) => {
+const DisabledUsernameInput = ({ ...props }) => {
   return (
     <div className="mt-4 flex w-2/3 flex-row items-center justify-center relative">
-      <span className="text-2xl font-normal mr-1">@</span>
+      <span className="text-2xl font-normal text-gray-500 mr-1">@</span>
       <input
         disabled
         type="text"
         className="border-2 border-gray-400 text-sm w-5/6 h-10 flex rounded-xl p-2"
-        placeholder={username}
+        placeholder={props.username}
       />
       <button className="absolute text-red-500" style={{ right: "-12px" }}>
-        <TrashIcon className="w-6 h-6" />
+        <TrashIcon
+          className="w-6 h-6"
+          onClick={async () => {
+            const [userData, hasError] = await updateUser({
+              linksMap: {
+                ...props.linksMapHook,
+                Twitter: props.linksMapHook.Twitter.filter(
+                  (item) => item !== props.username
+                ),
+              },
+            });
+            props.setLinksMapHook(userData.linksMap);
+            hasError
+              ? toast(`Error unlinking ${props.username}.`)
+              : toast(
+                  `Successfully unlinked Twitter account: ${props.username}.`
+                );
+          }}
+        />
       </button>
     </div>
   );
