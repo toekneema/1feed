44c84920de21f9ea9add4455b48fed9b62f677ba
Tony Ma <officialtonyma@gmail.com>
2022-02-15 00:35:29 -0500

    layout is better now, added a dummy div which serves as a spacer

--- frontend/src/pages/PublicProfilePage.js
+++ frontend/src/pages/PublicProfilePage.js
@@ -36,16 +36,16 @@ export const PublicProfilePage = () => {
         className="flex flex-row mt-16"
         style={{ marginLeft: "32rem", marginRight: "32rem" }}
       >
-        <div className="flex flex-col basis-2/5 border-2 border-red-500">
+        <div className="flex flex-col basis-2/6 border-2 border-red-500">
           <img
-            className="w-1/2 h-auto object-cover"
+            className="w-full h-auto object-cover"
             src={data[0].avatarUrl}
             alt="avatar"
           />
           <h3 className="mt-3 font-semibold text-xl">
             @{usernameCaseSensitive}
           </h3>
-          <div className="mt-6 text w-96" style={{ wordBreak: "break-word" }}>
+          <div className="mt-6 text w-full" style={{ wordBreak: "break-word" }}>
             {data[0].bio}
           </div>
           <div className="mt-10">
@@ -53,7 +53,7 @@ export const PublicProfilePage = () => {
             {Object.entries(data[0].linksMap).map(([key, value], idx) => (
               <div key={idx} className="mt-3">
                 <button
-                  className="p-2 border-2 border-black w-1/2"
+                  className="p-2 border-2 border-black w-full"
                   style={{ boxShadow: "4px 4px" }}
                 >
                   {key}
@@ -62,7 +62,8 @@ export const PublicProfilePage = () => {
             ))}
           </div>
         </div>
-        <div className="flex flex-col basis-3/5 items-end border-2 border-red-500">
+        <div className="basis-1/6" />
+        <div className="flex flex-col basis-3/6 items-end border-2 border-red-500">
           <div
             id="oEmbed_container"
             dangerouslySetInnerHTML={{ __html: fakeTweetHTML }}
