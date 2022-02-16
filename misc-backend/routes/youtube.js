3153265de1a09cdde8973d08c2d42a88c905e296
Tony Ma <officialtonyma@gmail.com>
2022-02-15 23:28:37 -0500

    update some comments

--- misc-backend/routes/youtube.js
+++ misc-backend/routes/youtube.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const YT_API_KEY = "AIzaSyCx9gQHRQQqQnc5oYBcRxEjJ4nqL-vb5R4";
 
-// Get last 5 YouTube video URLs using passed in channel ID
+// Get last 5 YouTube videoId and publishedAt dates using passed in channel ID
 router.get("/:id", (req, res) => {
   const channelId = req.params.id;
   const alteredChannelId = channelId.slice(0, 1) + "U" + channelId.slice(2);
@@ -12,15 +12,15 @@ router.get("/:id", (req, res) => {
   axios
     .get(requestURL)
     .then((response) => {
-      res.send(extractURLs(response.data.items));
+      res.send(extractIdsAndDate(response.data.items));
     })
     .catch((e) => {
       res.send(`Error fetching YouTube videos: ${e}`);
     });
 });
 
-// Takes in an array of video items, and extracts the video ID of each of them and turns them into a full YouTube video URL
-const extractURLs = (items) => {
+// Takes in an array of video items, and extracts the videoId and the publishedAt fields
+const extractIdsAndDate = (items) => {
   let urls = [];
   items.forEach((item) => {
     urls.push({
