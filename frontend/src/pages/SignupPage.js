ce585c115ddda62027ab18e124d079df8f10731c
Tony Ma <officialtonyma@gmail.com>
2022-02-19 00:41:07 -0500

    progress on sign up page design

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { LinkIcon } from "@heroicons/react/solid";
+import { CheckIcon } from "@heroicons/react/solid";
 import { register } from "../services/auth";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
@@ -32,19 +32,23 @@ export const SignupPage = () => {
   };
   return (
     <>
-      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full">
-          <div>
-            <img
-              className="mx-auto h-12 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-              alt="Workflow"
-            />
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Sign Up for an account
-            </h2>
-          </div>
+      <div className="min-h-full flex items-center justify-center mt-16 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-lg w-full">
+          <h2
+            className="text-black font-semibold text-center"
+            style={{ fontFamily: "Inter", fontSize: "36px" }}
+          >
+            Welcome to{" "}
+            <span className="text-gray-400 hover:text-indigo-500">onefeed</span>
+          </h2>
+          <h2
+            className="text-black font-semibold -mt-3 mb-4 text-center"
+            style={{ fontFamily: "Inter", fontSize: "36px" }}
+          >
+            Start by naming your feed
+          </h2>
           <form
+            className="mt-16 space-y-16"
             onSubmit={async (event) => {
               event.preventDefault();
               const [error, hasError] = await register(
@@ -55,38 +59,31 @@ export const SignupPage = () => {
               hasError ? toast(error) : navigate("/edit");
             }}
           >
-            <div className="mt-8 rounded-md relative shadow-sm">
-              <input
-                id="username"
-                placeholder="Username"
-                onChange={(event) => {
-                  usernameIsValid(event.target.value);
-                }}
-                className="pl-32 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                style={{ paddingLeft: "8.3rem" }}
-              />
-              <div className="absolute items-center left-3 top-0 bottom-0 z-50 flex">
-                <LinkIcon className="h-6 w-6 text-indigo-600" />
-                <p className="pl-1 tracking-wide font-bold text-gray-700">
-                  1feed.com/
-                </p>
+            <div>
+              <div className="relative">
+                <input
+                  id="username"
+                  placeholder="Username"
+                  onChange={(event) => {
+                    usernameIsValid(event.target.value);
+                  }}
+                  className="pl-32 appearance-none tracking-wider w-full relative px-3 py-2 placeholder:text-gray-500 border-2 border-gray-500 focus:outline-black sm:text-sm"
+                  style={{ textTransform: "uppercase" }}
+                />
+                <div className="absolute items-center left-3 top-0 bottom-0 z-50 flex tracking-wide font-bold text-gray-700 pl-1">
+                  1FEED.COM/
+                </div>
               </div>
+
+              {usernameErrorMsg !== "valid" && (
+                <p className="text-red-500 ml-2" style={{ fontSize: "13px" }}>
+                  {usernameErrorMsg}
+                </p>
+              )}
             </div>
 
-            {usernameErrorMsg !== "valid" && (
-              <p
-                className="text-red-500 ml-2 mb-5"
-                style={{ fontSize: "13px" }}
-              >
-                {usernameErrorMsg}
-              </p>
-            )}
-
-            <div className="rounded-md shadow-sm -space-y-px mb-6">
-              <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
+            <div>
+              <div className="mb-3">
                 <input
                   onChange={(event) => setEmail(event.target.value)}
                   id="email-address"
@@ -94,14 +91,11 @@ export const SignupPage = () => {
                   type="email"
                   autoComplete="email"
                   required
-                  className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Email address"
+                  className="tracking-wider w-full px-3 py-2 placeholder:text-gray-500 border-2 border-gray-500 focus:outline-black sm:text-sm"
+                  placeholder="EMAIL ADDRESS"
                 />
               </div>
               <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
                 <input
                   onChange={(event) => setPassword(event.target.value)}
                   id="password"
@@ -109,25 +103,25 @@ export const SignupPage = () => {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Password"
+                  className="tracking-wider w-full px-3 py-2 placeholder:text-gray-500 border-2 border-gray-500 focus:outline-black sm:text-sm"
+                  placeholder="PASSWORD"
                 />
               </div>
             </div>
 
-            <div className="mb-6">
+            <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-full flex tracking-widest justify-center py-2 px-4 border border-transparent text-sm font-medium text-white bg-black hover:bg-indigo-500 focus:outline-none"
               >
-                Create account
+                CREATE ACCOUNT
               </button>
             </div>
           </form>
-          <div className="flex items-center justify-center text-sm">
+          <div className="flex items-center justify-center text-sm mt-6">
             <a
               href="/login"
-              className="flex items-center font-medium text-indigo-600 hover:text-indigo-500"
+              className="flex items-center font-medium hover:text-indigo-500"
             >
               Already have an acccount? Login
             </a>
